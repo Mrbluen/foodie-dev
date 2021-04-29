1ca979be5ca170ddcab5dc1cb7916ddc1f4d363a
chad <8980192+charlychang@user.noreply.gitee.com>
2021-04-29 19:17:01 +0800

    初次提交

---
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/StuServiceImpl.java
@@ -0,0 +1,43 @@
+package com.imooc.service.impl;
+
+import com.imooc.mapper.StuMapper;
+import com.imooc.pojo.Stu;
+import com.imooc.service.StuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class StuServiceImpl implements StuService {
+    @Autowired
+    private StuMapper stuMapper;
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Stu getStuInfo(int id) {
+        return stuMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void saveStu() {
+        Stu stu = new Stu();
+        stu.setName("jack");
+        stu.setId(15);
+        stuMapper.insert(stu);
+    }
+
+    @Override
+    public void updateStu(int id) {
+        Stu stu = new Stu();
+        stu.setId(id);
+        stu.setName("tom");
+        stu.setAge(25);
+        stuMapper.updateByPrimaryKeySelective(stu);
+    }
+
+    @Override
+    public void deleteStu(int id) {
+        stuMapper.deleteByPrimaryKey(id);
+    }
+
+}
