8b66c8604e11bcea851defdbe07aa3de441aac51
1195643053@qq.com <28iZHbXpHk!TNB5!hqab>
2021-05-06 09:07:44 +0800

    3-10.跨域的请求配置

---
+++ foodie-dev-api/src/main/java/com/imooc/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.imooc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    public CorsConfig(){
+
+    }
+    @Bean
+    public CorsFilter corsFilter(){
+        //1.添加cors配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        //为url添加映射路径
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        corsSource.registerCorsConfiguration("/**",config);
+
+        //3返回重新定义好的corsSource
+        return new CorsFilter(corsSource);
+    }
+}
