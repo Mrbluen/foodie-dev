81e53b70653a10acd65f82b9bf6a4d265a1e97bc
1195643053@qq.com <28iZHbXpHk!TNB5!hqab>
2021-05-06 23:01:22 +0800

    1-2，轮播图的实现

---
+++ foodie-dev-service/src/main/java/com/imooc/service/CarouselService.java
@@ -0,0 +1,10 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Carousel;
+
+import java.util.List;
+
+public interface CarouselService {
+    //查询所有轮播图列表
+    public List<Carousel> queryAll(Integer isShow);
+}
