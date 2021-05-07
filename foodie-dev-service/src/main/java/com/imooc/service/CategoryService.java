fef1e2a507c5a55d8a13a856b52af20ac6019c81
1195643053@qq.com <28iZHbXpHk!TNB5!hqab>
2021-05-07 09:23:47 +0800

    1-5，自定义mapper实现懒加载子分类查询

--- foodie-dev-service/src/main/java/com/imooc/service/CategoryService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/CategoryService.java
@@ -1,11 +1,14 @@
-package com.imooc.service.impl;
+package com.imooc.service;
 
-import com.imooc.pojo.Carousel;
 import com.imooc.pojo.Category;
+import com.imooc.pojo.vo.CategoryVO;
 
 import java.util.List;
 
 public interface CategoryService {
     //查询所有轮播图列表
     public List<Category> queryAllRootLevelCat();
+    //根据一级分类信息查询子分类信息
+    public List<CategoryVO> getSubCatList(Integer rootCatId);
+
 }
