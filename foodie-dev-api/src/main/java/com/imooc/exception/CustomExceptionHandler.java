4482e18d6a857134e459acc77d2fb63c0fbf8525
orange <email@e1195643053@qq.com>
2021-05-26 10:22:52 +0800

    用户中心-上传文件超过500kb，捕获异常

---
+++ foodie-dev-api/src/main/java/com/imooc/exception/CustomExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.imooc.exception;
+
+import com.imooc.utils.IMOOCJSONResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+    //上传文件超过500kb，捕获异常。
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public IMOOCJSONResult handlerMaxUploadFile(MaxUploadSizeExceededException ex){
+        return IMOOCJSONResult.errorMsg("文件图片上传大小不能大于500kb");
+    }
+
+
+}
